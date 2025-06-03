31fbd494c43cfa9e19c71df0636b5c3f5bfb8d10
Alex070800 <belka.aly2000@gmail.com>
2025-06-03 19:19:47 +0300

    Тест публикации на github

--- src/mocks/values/day-mock.ts
+++ src/mocks/values/day-mock.ts
@@ -3,7 +3,7 @@ import { Meal } from "../view-models/meal";
 
 export const dayMock = new Day(new Date(), [
   new Meal(1, 1, "Завтрак", "Лёша", 20),
-  new Meal(2, 2, "Обед", "Алёна", 40),
+  new Meal(2, 2, "Обед", "Алёна", 80),
   new Meal(3, 3, "Полдник", "Алёна", 10),
   new Meal(4, 4, "Ужин", "Алёна", 25),
 ]);
